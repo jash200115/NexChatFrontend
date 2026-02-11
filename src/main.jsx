3bec3c410a4897d434c5c9f23b277258ec282789
jash200115 <jashroy29@gmail.com>
2026-02-11 23:57:20 +0530

    Typo correction

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { HashRouter } from "react-router-dom";
-import AppRoutes from "./config/routes.jsx";
+import AppRoutes from "./config/Routes.jsx";
 import { Toaster } from "react-hot-toast";
 import { ChatProvider } from "./context/ChatContext.jsx";
 
